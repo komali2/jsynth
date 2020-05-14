9a791864f7f1b693192c2dacd653b929607d9463
Caleb Rogers <rogersjcaleb@gmail.com>
2020-05-14 09:47:58 -0700

    Working towards getting first synth set up

--- playground.js
+++ playground.js
@@ -18,25 +18,70 @@ function app() {
 
 function osc1() {
   const oscillator = window.audioCtx.createOscillator();
-  oscillators.osc1 = oscillator;
-  oscillator.type = 'square';
-  oscillator.frequency.setValueAtTime(440, window.audioCtx.currentTime); // value in hertz
-  oscillator.connect(window.audioCtx.destination);
+  oscillators.osc1 = new Synth(1);
 }
 
-function stop(osc) {
-  oscillators[osc].stop();
-}
 
-function start(osc) {
-  oscillators[osc].start();
-}
-
-function setUpListeners(osc) {
-  document.querySelector(`${osc} button.start`).addEventListener('click', () => {
-    stop(osc);
+function attachListeners(osc) {
+  console.log(osc);
+  document.querySelector(`.${osc} button.stop`).addEventListener('click', () => {
+    oscillators[osc].stopOsc();
   });
-  document.querySelector(`${osc} button.start`).addEventListener('click', () => {
-    start(osc);
+  document.querySelector(`.${osc} button.start`).addEventListener('click', () => {
+    oscillators[osc].startOsc();
   });
+  document.querySelectorAll(`.${osc} button.volume`).forEach( (el) => {
+    console.log(el);
+      // el.addEventListener('click', (e) => {
+      //   const direction = e.toElement.dataset.direction;
+      //   oscillators[osc].volumeIterate(direction);
+      // });
+  });
+function setUpListeners() {
+  Object.keys(oscillators).forEach(osc => attachListeners(osc));
+}
+
+class Synth {
+  constructor(id) {
+    this.id = id;
+    this.createOsc();
+    this.createGainNode();
+    this.connectNodes();
+  }
+  createOsc() {
+    this.oscillator = window.audioCtx.createOscillator();
+    this.oscillator.type = 'square';
+    this.oscillator.frequency.setValueAtTime(440, window.audioCtx.currentTime); // value in hertz
+  }
+  createGainNode() {
+    this.gain = window.audioCtx.createGain();
+  }
+  connectNodes() {
+    this.oscillator.connect(this.gain);
+    this.gain.connect(window.audioCtx.destination);
+  }
+  stopOsc() {
+    this.oscillator.stop();
+  }
+  startOsc() {
+    this.oscillator.start();
+  }
+  volumeUp() {
+    const currentVolume = this.gain.gain.value;
+    this.gain.gain.setValueAtTime(currentVolume + .1, window.audioCtx.currentTime);
+  }
+  volumeDown() {
+    const currentVolume = this.gain.gain.value;
+    this.gain.gain.setValueAtTime(currentVolume + .1, window.audioCtx.currentTime);
+  }
+  volumeIterate(direction) {
+    switch (direction) {
+      case 'up':
+        this.volumeUp();
+        break;
+      case 'down':
+        this.volumeDown();
+        break;
+    }
+  }
 }
